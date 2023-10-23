b3d054f54a09e439555e99498d9816d024eed0be
seoljoo-kang <seoljoo.kang@gmail.com>
2023-10-23 15:42:37 -0400

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import { AppPropsWithLayout } from "../types"
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
-// import { Analytics } from "@vercel/analytics/react"
+
+// eslint-disable-next-line import/no-unresolved 
+import { Analytics } from "@vercel/analytics/react"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
@@ -12,7 +14,7 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
       <Hydrate state={pageProps.dehydratedState}>
         <RootLayout>{getLayout(
           <><Component {...pageProps} />
-          {/* <Analytics /> */}
+          <Analytics />
           </>
         )}</RootLayout>
       </Hydrate>
