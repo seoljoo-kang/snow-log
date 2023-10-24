9aa4c93a6e8789620ad3c2383eaea38accd0337a
seoljoo-kang <seoljoo.kang@gmail.com>
2023-10-23 22:59:55 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
+
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  
 }
